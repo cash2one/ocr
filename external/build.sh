1d23fdb3e2c40e9885c096245ee17df46b97e285
xuyifei <xuyifei@baidu.com>
2016-12-15 13:00:40 +0800

    update case and subscribe to external
    Change-Id: Ie5c946dcc8e36ebbc17507e695e030546de0fa5e

--- external/build.sh
+++ external/build.sh
@@ -5,7 +5,7 @@ mkdir -p ${OUTPUT_DIR}
 BUILD_SUB_DIRS="app conf data template webroot"
 
 cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
-tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
-mv ${OUTPUT_DIR}.tar ../
+#tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
+#mv ${OUTPUT_DIR}.tar ../
 
 echo "build all done!"
