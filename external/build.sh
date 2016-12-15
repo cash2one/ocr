9a18197c8f46386f730636f4770c965859ee2a6b
xuyifei <xuyifei@baidu.com>
2016-12-15 11:25:04 +0800

    add agile build
    Change-Id: Ide7b66b537314c770495cc88108b36ca64613446

--- external/build.sh
+++ external/build.sh
@@ -6,5 +6,6 @@ BUILD_SUB_DIRS="app conf data template webroot"
 
 cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
 tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
+mv ${OUTPUT_DIR}.tar ../
 
 echo "build all done!"
