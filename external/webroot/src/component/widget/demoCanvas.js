073a4b5720b713addb6eac7c74201112979f2816
Shi-Liang <385936547@qq.com>
2016-12-22 18:20:33 +0800

    人脸检测完成 & build

--- external/webroot/src/component/widget/demoCanvas.js
+++ external/webroot/src/component/widget/demoCanvas.js
@@ -8,16 +8,16 @@ import $ from 'jquery';
 import {getHeader} from '../../model/demoAPI';
 
 export default class DemoCanvas {
-    constructor({selector, image, type, toCheck = true, success, fail}) {
+    constructor({selector, image, type, toCheck = true, scale = 1, success, fail}) {
         if (!$(selector).context) {
             throw 'DemoCanvas：未寻找到容器!';
         }
         this.container = $(selector);
         this.type = type;
+        this.scale = scale;
         this.image = new Image();
         this.success = success || $.noop;
         this.fail = fail || $.noop;
-        this.image.onload = () => this.render();
 
         this.image.onerror = () => {
             this.fail();
@@ -32,17 +32,24 @@ export default class DemoCanvas {
 
             $.when(promise).then(
                 resultImg => {
+                    this.image.onload = () => {
+                        this.render(true);
+                    }
                     this.image.src = resultImg;
-                    this.success(this.image.src);
                 },
                 errorImg => {
+                    this.image.onload = () => {
+                        this.render(false);
+                    }
                     this.image.src = errorImg;
-                    this.fail();
+
                 }
             );
         } else {
+            this.image.onload = () => {
+                this.render(true);
+            }
             this.image.src = image;
-            this.success(this.image.src);
         }
 
     }
@@ -80,6 +87,10 @@ export default class DemoCanvas {
     checkByStream(image) {
         let dfd = $.Deferred();
         let reader = new FileReader();
+        if (!image) {
+            dfd.reject('/images/error/not-found.png');
+            return dfd.promise();
+        }
         reader.readAsDataURL(image);
         reader.onload = e => {
             if (!/image\/(png|bmp|jpeg)/.test(image.type)) {
@@ -98,13 +109,13 @@ export default class DemoCanvas {
         return dfd.promise();
     }
 
-    render() {
+    render(isSuccessful) {
         let cWidth = this.container.width();
         let cHeight = this.container.height();
         let iWidth = this.image.width;
         let iHeight = this.image.height;
 
-        let canvas = $('<canvas>').attr('width', iWidth)
+        let canvas = $('<canvas>您的浏览器暂时不支持canvas，请选用现代浏览器！</canvas>').attr('width', iWidth)
             .attr('height', iHeight);
         let ctx = canvas[0].getContext('2d');
         ctx.drawImage(this.image, 0, 0);
@@ -112,9 +123,9 @@ export default class DemoCanvas {
         let wRatio = iWidth / cWidth;
         let hRatio = iHeight / cHeight;
 
-        let scaleRatio = (wRatio > 1 || hRatio > 1)
+        let scaleRatio = this.scale * ((wRatio > 1 || hRatio > 1)
                 ? (1 / (wRatio >= hRatio ? wRatio : hRatio))
-                : 1;
+                : 1);
 
         canvas.css({
             'position': 'relative',
@@ -127,5 +138,12 @@ export default class DemoCanvas {
         });
         canvas.attr('data-scale', scaleRatio);
         this.container.empty().append(canvas);
+        if (isSuccessful) {
+            this.success(this.image.src);
+        }
+        else {
+            this.fail();
+        }
+
     }
 }
