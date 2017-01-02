522e8f1a4ac32242248b44631337912a98092ba5
xuyifei <xuyifei@baidu.com>
2017-01-02 18:52:31 +0800

    update build dir
    Change-Id: Id98e81787e9e698e8fe491c10fef05d035dffb3d

--- external/build.sh
+++ external/build.sh
@@ -2,7 +2,7 @@
 OUTPUT_DIR=output
 mkdir -p ${OUTPUT_DIR}
 
-BUILD_SUB_DIRS="app conf data template webroot"
+BUILD_SUB_DIRS="app conf data template webroot scripts tool"
 
 cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
 #tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
