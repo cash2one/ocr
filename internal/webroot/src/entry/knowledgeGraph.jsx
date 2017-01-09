1a70abd66062269e979b091373e835bc582af01c
Shi-Liang <385936547@qq.com>
2017-01-09 14:20:38 +0800

    内部版链接修正，对于js中的部分图片链接

--- internal/webroot/src/entry/knowledgeGraph.jsx
+++ internal/webroot/src/entry/knowledgeGraph.jsx
@@ -5,17 +5,17 @@ var ReactDOM = require('react-dom');
 var Carousel = require('../component/common/Carousel');
 
 var carouselContent = [{
-    label: '搜索知识问答', image: '../images/knowledge-scroll-1@2x.jpg'
+    label: '搜索知识问答', image: '/internal/images/knowledge-scroll-1@2x.jpg'
 }, {
-    label: '搜索推荐', image: '../images/knowledge-scroll-2@2x.jpg'
+    label: '搜索推荐', image: '/internal/images/knowledge-scroll-2@2x.jpg'
 }, {
-    label: '实体卡片', image: '../images/knowledge-scroll-3@2x.jpg'
+    label: '实体卡片', image: '/internal/images/knowledge-scroll-3@2x.jpg'
 }, {
-    label: '闪投广告触发优化', image: '../images/knowledge-scroll-4@2x.jpg'
+    label: '闪投广告触发优化', image: '/internal/images/knowledge-scroll-4@2x.jpg'
 }, {
-    label: '糯米酒店样式优化', image: '../images/knowledge-scroll-5@2x.jpg'
+    label: '糯米酒店样式优化', image: '/internal/images/knowledge-scroll-5@2x.jpg'
 }, {
-    label: '凤巢天问项目', image: '../images/knowledge-scroll-6@2x.jpg'
+    label: '凤巢天问项目', image: '/internal/images/knowledge-scroll-6@2x.jpg'
 }];
 
 ReactDOM.render(
--- internal/webroot/src/entry/naturalLanguage.jsx
+++ internal/webroot/src/entry/naturalLanguage.jsx
@@ -5,13 +5,13 @@ var ReactDOM = require('react-dom');
 var Carousel = require('../component/common/Carousel');
 
 var carouselContent = [{
-    label: '度秘对话交互', image: '../images/natural-scroll-1@2x.jpg'
+    label: '度秘对话交互', image: '/internal/images/natural-scroll-1@2x.jpg'
 }, {
-    label: '新闻摘要生成', image: '../images/natural-scroll-2@2x.jpg'
+    label: '新闻摘要生成', image: '/internal/images/natural-scroll-2@2x.jpg'
 }, {
-    label: '百度会话翻译', image: '../images/natural-scroll-3@2x.jpg'
+    label: '百度会话翻译', image: '/internal/images/natural-scroll-3@2x.jpg'
 }, {
-    label: '百度糯米评论分析', image: '../images/natural-scroll-4@2x.jpg'
+    label: '百度糯米评论分析', image: '/internal/images/natural-scroll-4@2x.jpg'
 }];
 
 ReactDOM.render(
