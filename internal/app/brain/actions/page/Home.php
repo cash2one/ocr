56595f730920130e1804e77f92549992f2e211d4
xuyifei <xuyifei@baidu.com>
2017-01-10 14:15:06 +0800

    add uuap check to internal
    Change-Id: Ia2b4746ec7c63c226e6fa5464c74508fc6f64c3e

--- internal/app/brain/actions/page/Home.php
+++ internal/app/brain/actions/page/Home.php
@@ -16,6 +16,8 @@
 class Action_Home extends Ap_Action_Abstract {
 
     public function execute() {
+
+        !Brain_User::checkUuapLogin() && exit(0);
 //        $arrPageInfo['messagelist'] = Bd_Conf::getAppConf('messagelist');
         Brain_Output::htmlOutput(
             $arrPageInfo, 
