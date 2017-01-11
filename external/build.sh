2b8258b272f6c3788cea7958be259e5b7ccf8fc7
zhouyingfeng <zhouyingfeng@baidu.com>
2017-01-11 19:22:37 +0800

    build test
    Change-Id: I829eda6e1ceef75f53244772402893ad7b35960d

--- external/build.sh
+++ external/build.sh
@@ -2,12 +2,13 @@
 OUTPUT_DIR=output/
 mkdir -p ${OUTPUT_DIR}
 
-sh invoke_jenkins_job.sh
+#sh invoke_jenkins_job.sh
 
-BUILD_SUB_DIRS="app conf data template webroot scripts webserver"
+#BUILD_SUB_DIRS="app conf data template webroot scripts webserver"
 
-cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
+#cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
 #tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
 #mv ${OUTPUT_DIR}.tar ../
 
 echo "build all done!"
+export
