cfb93e39d6a6219bb72beb40b29b074a3b6ca64c
Franckchen <chenfan02@baidu.com>
2017-01-11 21:11:55 +0800

    代码风格

--- external/webroot/devBuild/build.js
+++ external/webroot/devBuild/build.js
@@ -1,7 +1,14 @@
+/**
+ * @file 构建任务
+ * @author Franck Chen(chenfan02@baidu.com)
+ */
+
+/* eslint-disable */
 const webpack = require('webpack');
 const argv = require('minimist')(process.argv.slice(2));
 
 let webpackConfig = require('./webpack.config');
+/* eslint-enable */
 
 const isWatchMode = argv.hasOwnProperty('w');
 
