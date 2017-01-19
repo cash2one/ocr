346c38d2a5054cf9ff73f2378ae547911c258df5
xuyifei <xuyifei@baidu.com>
2017-01-19 18:40:54 +0800

    update deploy.sh
    Change-Id: Iea88ef7f34d4908544e121ca5be1a68650722a82

--- external/scripts/deploy.sh
+++ external/scripts/deploy.sh
@@ -1,16 +1,13 @@
-cd ../webroot/deploy_packages
-sh deploy.sh
-
-cd ../
+cd ../webroot/
 mkdir download
 cd download
-rm -f aip-antiporn-java-sdk-1.0.zip  aip-face-java-sdk-1.0.zip  aip-nlp-java-sdk-1.0.zip  aip-ocr-java-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-java-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-java-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-java-sdk-1.0.zip 
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-java-sdk-1.0.zip
-rm -f aip-antiporn-php-sdk-1.0.zip   aip-face-php-sdk-1.0.zip   aip-nlp-php-sdk-1.0.zip   aip-ocr-php-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-php-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-php-sdk-1.0.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-php-sdk-1.0.zip 
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-php-sdk-1.0.zip
+rm -f aip-antiporn-java-sdk-1.1.zip aip-antiporn-php-sdk-1.1.zip aip-face-java-sdk-1.1.zip aip-face-php-sdk-1.1.zip aip-nlp-java-sdk-1.1.zip aip-nlp-php-sdk-1.1.zip aip-ocr-java-sdk-1.1.zip aip-ocr-php-sdk-1.1.zip aip-python-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-java-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-php-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-java-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-php-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-java-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-php-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-java-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-php-sdk-1.1.zip
+wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-python-sdk-1.1.zip
