11b94a62e643012cac84ebdc3fd7bd0c2c64b8b1
chenweiwei01 <chenweiwei01@baidu.com>
2017-02-27 17:03:56 +0800

    人脸查找页面重构
    Change-Id: Iecbf040d21d1c4eab2b115a6ee5e17ed2a637625

--- external/webroot/src/entry/technology/face-search.js
+++ external/webroot/src/entry/technology/face-search.js
@@ -10,39 +10,39 @@ import 'less/technology/face-search.less';
 
 import '../common/tech-case.js';
 
-$(document).ready(function () {
 
-    // 触发功能介绍动画
-    $(window).scroll(
-        throttle(
-            () => {
-                if ($(document).scrollTop() >= 100) {
-                    $('.tech-function-demo').trigger('demo');
-                }
-            },
-            300
-        )
+const $demo = $('.tech-function-demo');
+// 触发功能介绍动画
+$(window).scroll(
+    throttle(
+        () => {
+            if ($(document).scrollTop() >= 100) {
+                $demo.trigger('demo');
+            }
+        },
+        300
+    )
+);
+
+// 绑定功能介绍动画
+$demo.one('demo', function () {
+    let counter = 0;
+    let faces = $('.tech-function-demo-item');
+
+    let interval = setInterval(
+        function () {
+            faces
+                .removeClass('tech-function-scanning')
+                .eq(counter)
+                .addClass('tech-function-scanning');
+
+            if (counter++ === faces.length) {
+                faces.addClass('tech-function-scanned');
+
+                clearInterval(interval);
+            }
+        },
+        700
     );
-
-    // 绑定功能介绍动画
-    $('.tech-function-demo').one('demo', function () {
-        let counter = 0;
-        let faces = $('.tech-function-demo-item');
-
-        let interval = setInterval(
-            function () {
-                faces
-                    .removeClass('tech-function-scanning')
-                    .eq(counter)
-                    .addClass('tech-function-scanning');
-
-                if (counter++ === faces.length) {
-                    faces.addClass('tech-function-scanned');
-
-                    clearInterval(interval);
-                }
-            },
-            700
-        );
-    });
 });
+
