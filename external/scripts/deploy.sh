2982e9e20287a9801225de486a993841d7d12542
songqingyun <songqingyun@baidu.com>
2017-03-01 16:39:39 +0800

    deploy.sh
    Change-Id: I4be6c0502d73c4f6b53770bc2345a7579a445923

--- external/scripts/deploy.sh
+++ external/scripts/deploy.sh
@@ -2,12 +2,11 @@ cd ../webroot/
 mkdir download
 cd download
 rm -f aip-antiporn-java-sdk-1.1.zip aip-antiporn-php-sdk-1.1.zip aip-face-java-sdk-1.1.zip aip-face-php-sdk-1.1.zip aip-nlp-java-sdk-1.1.zip aip-nlp-php-sdk-1.1.zip aip-ocr-java-sdk-1.1.zip aip-ocr-php-sdk-1.1.zip aip-python-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-java-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-antiporn-php-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-java-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-face-php-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-java-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-nlp-php-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-java-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-ocr-php-sdk-1.1.zip
-wget ftp://szwg-qatest-dpf007.szwg01.baidu.com/home/work/yifei/ai/sdk/aip-python-sdk-1.1.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-antiporn-java-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-antiporn-php-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-face-java-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-face-php-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-nlp-java-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-ocr-java-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-ocr-php-sdk-1.2.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-python-sdk-1.2.zip
