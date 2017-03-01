0ea5563c1737872cbfb079a4a6e1f843f1222cfb
songqingyun <songqingyun@baidu.com>
2017-03-01 16:40:34 +0800

    deploy.sh
    Change-Id: Id0bcf55960d1df64706f3c4c46a9f2d478cb41a3

--- external/scripts/deploy.sh
+++ external/scripts/deploy.sh
@@ -1,7 +1,7 @@
 cd ../webroot/
 mkdir download
 cd download
-rm -f aip-antiporn-java-sdk-1.1.zip aip-antiporn-php-sdk-1.1.zip aip-face-java-sdk-1.1.zip aip-face-php-sdk-1.1.zip aip-nlp-java-sdk-1.1.zip aip-nlp-php-sdk-1.1.zip aip-ocr-java-sdk-1.1.zip aip-ocr-php-sdk-1.1.zip aip-python-sdk-1.1.zip
+rm -f aip-antiporn-java-sdk-1.2.zip aip-antiporn-php-sdk-1.2.zip aip-face-java-sdk-1.2.zip aip-face-php-sdk-1.2.zip aip-nlp-java-sdk-1.2.zip aip-nlp-php-sdk-1.2.zip aip-ocr-java-sdk-1.2.zip aip-ocr-php-sdk-1.2.zip aip-python-sdk-1.2.zip
 wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-antiporn-java-sdk-1.2.zip
 wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-antiporn-php-sdk-1.2.zip
 wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.2/aip-face-java-sdk-1.2.zip
