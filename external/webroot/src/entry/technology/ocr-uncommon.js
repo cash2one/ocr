73232536b45f20a99ed744bea220d6fb0d01b229
chenweiwei01 <chenweiwei01@baidu.com>
2017-03-01 19:12:37 +0800

    生僻字20170301
    Change-Id: I86091bae823afab12ecc144a2b98f6296d875392

--- external/webroot/src/entry/technology/ocr-uncommon.js
+++ external/webroot/src/entry/technology/ocr-uncommon.js
@@ -6,63 +6,144 @@
 
 import $ from 'jquery';
 
+import '../common/tech-case'
 import '../../less/technology/ocr-uncommon.less';
-import DemoCanvas from '../../component/widget/demoCanvas';
 import {scanGeneralText} from '../../model/demoAPI';
 
-
+// 获取信息
+const $getUrlType = (
+    {
+        init:
+            function() {
+                if(!this._xmlHttp) {
+                    if (window.ActiveXObject){
+                        this._xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+                    }
+                    else {
+                        this._xmlHttp = new XMLHttpRequest();
+                    }
+                }
+            },
+        get:
+            function(url, params, callback) {
+                this.init();
+                try{
+                    this._xmlHttp.open('GET', url, true);
+                    this._xmlHttp.setRequestHeader(
+                        "Content-Type",
+                        "application/x-www-form-urlencoded"
+                    );
+                    this._xmlHttp.onreadystatechange = function() {
+                        if(this.readyState == 4 && this.status == 200) {
+                            const header = this.getAllResponseHeaders();
+                            callback(header);
+                        }
+                    };
+                    this._xmlHttp.send(params);
+                }
+                catch(e){
+                    alert(e.message);
+                }
+            }
+    }
+);
 
 // Demo实例图片
-const $techDemoSelect = $('.tech-demo-examples img');
+const $techDemoSelect = $('.tech-demo-card-item');
 
 // 实例图片点击点击
 $techDemoSelect.on('click', ({target}) => {
     const $target = $(target);
 
-    if ($target.hasClass('active')) {
+    if ($target
+            .parent()
+            .hasClass('tech-demo-card-active')){
         return;
     }
 
     $target
-        .addClass('active')
+        .parent()
+        .addClass('tech-demo-card-active')
         .siblings()
-        .removeClass('active');
-
-    let demoUrl = `${window.location.protocol}${$target.attr('src')}`;
-    new DemoCanvas({
-        selector: '#demo-origin',
-        image: demoUrl,
-        type: 'url',
-        toCheck: false,
-        success(imgSrc) {
-            startScan('url', imgSrc, demoUrl);
-        },
-    });
+        .removeClass('tech-demo-card-active');
+    const demoUrl = `${window.location.protocol}${$target.attr('src')}`;
+    getOriginImg(demoUrl)
 });
 
-const startScan = function(type, imgSrc, url) {
-    let options = {
+// 使用url上传图片
+const $monitorUrlBtn = $('#scan-photo');
+const $demoPhotoUrl = $('#demo-photo-url');
+$monitorUrlBtn.on('click',
+    () => {
+        const demoUrl = $demoPhotoUrl.val();
+        if(!/\.(jpe?g|png|gif)$/.test(demoUrl)){
+            return;
+        }
+        todoUrl(demoUrl)
+    }
+);
+
+// 判断链接是否有效
+const todoUrl = function(demoUrl) {
+    $getUrlType.get(
+        demoUrl,
+        '',
+        function(data){
+            if(data.indexOf('image') >= 0) {
+
+            }
+        }
+    );
+};
+
+// 获取图片
+const getOriginImg = function(imgUrl){
+    const demoOrigin = $('#demo-origin');
+
+    demoOrigin.html(
+        "<img class='tech-demo-origin-img' src='" + imgUrl + "'>"
+    );
+
+    startScan(imgUrl)
+};
+
+// 调用接口
+const startScan = function(imgUrl) {
+    const options = {
         success(res) {
-            $('#demo-json').html('<p>' + JSON.stringify(res, null, '\t') + '</p>');
-            let result = res.data.words_result;
-            result.forEach(
-                (value,key) => {
-                    $('.demo-result-table').append([
-                        '<tr>' +
-                        '<td>' + (key + 1) + '</td>',
-                        '<td>' + value.words + '</td>',
-                        '<td>' + value.location.left + '</td>',
-                        '<td>' + value.location.top + '</td>',
-                        '<td>' + value.location.width + '</td>',
-                        '<td>' + value.location.height + '</td>',
-                        '</tr>'
-                    ].join(''));
-                }
-            );
+            console.log(res);
+            $('#demo-json').html(JSON.stringify(res, null, '\t'));
         }
     };
-    options.imageUrl = url;
+    options.imageUrl = imgUrl;
     scanGeneralText(options);
 };
+
+
+
+// const startScan = function(type, imgSrc, url) {
+//     let options = {
+//         success(res) {
+//             $('#demo-json').html('<p>' + JSON.stringify(res, null, '\t') + '</p>');
+//             let result = res.data.words_result;
+//             result.forEach(
+//                 (value,key) => {
+//                     $('.demo-result-table').append([
+//                         '<tr>' +
+//                         '<td>' + (key + 1) + '</td>',
+//                         '<td>' + value.words + '</td>',
+//                         '<td>' + value.location.left + '</td>',
+//                         '<td>' + value.location.top + '</td>',
+//                         '<td>' + value.location.width + '</td>',
+//                         '<td>' + value.location.height + '</td>',
+//                         '</tr>'
+//                     ].join(''));
+//                 }
+//             );
+//         }
+//     };
+//     options.imageUrl = url;
+//     scanGeneralText(options);
+// };
 
 // $techDemoSelect.click();
