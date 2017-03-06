7d503c908dbae63f797e4d58f0c013ed6b1b6ca0
songqingyun <songqingyun@baidu.com>
2017-03-06 16:26:14 +0800

    ar
    Change-Id: I432ee8ceaac28d3d14f199997fcb43677321a085

---
+++ external/app/brain/modules/Tech/controllers/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by songqingyun@baidu.com.
+ * User: songqingyun
+ * Date: 2017/3/3
+ * Time: 下午6:54
+ */
+class Controller_Ar extends Ap_Controller_Abstract
+{
+    public $actions = array(
+
+        'ar' => 'actions/tech/ar/Ar.php',
+        'antiporn' => 'actions/tech/antiporn/Antiporn.php',
+    );
+}
