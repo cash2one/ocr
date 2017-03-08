82c6509ef2492a144f323412569f7bd93536ba8d
songqingyun <songqingyun@baidu.com>
2017-03-08 20:55:42 +0800

    build
    Change-Id: I4e1dd663724fa3fa1763fea2f4b8a76b0fb3452f

--- external/build.sh
+++ external/build.sh
@@ -1,13 +1,22 @@
 #/bin/sh
-OUTPUT_DIR=output/
-mkdir -p ${OUTPUT_DIR}
+workspace=$(pwd)
+project="aip-platform-ext"
+output="$workspace/output"
+
+mkdir -p $output
 
 sh invoke_jenkins_job.sh
 
 BUILD_SUB_DIRS="app conf data template webroot scripts webserver"
 
-cp -rf ${BUILD_SUB_DIRS} ${OUTPUT_DIR}
-#tar -cf ${OUTPUT_DIR}.tar ${OUTPUT_DIR}/*
-#mv ${OUTPUT_DIR}.tar ../
-
+cp -rf ${BUILD_SUB_DIRS} ${output}
+cd ${output}
+    tgz="$project.tgz"
+  tar czf $tgz *
+mv $tgz $workspace/.
+  rm -rf $output/*
+  mv $workspace/$tgz $output/.
+  mv $workspace/deploy $output/.
+  sed -i "s/tgz=.*/&$tgz/g" $output/deploy
+  sed -i "s/BUILD_SUB_DIRS=.*/&\"$BUILD_SUB_DIRS\"/g" $output/deploy
 echo "build all done!"
