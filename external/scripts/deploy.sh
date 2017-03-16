74bd8a4997672872a99de20828ff2ada9956243c
songqingyun <songqingyun@baidu.com>
2017-03-16 21:02:27 +0800

    deploy
    Change-Id: Ia0b372d6338cbdd91a27202e4c09cff4b451a73c

--- external/scripts/deploy.sh
+++ external/scripts/deploy.sh
@@ -1,5 +1,4 @@
 cd ../webroot/
 cd download
-wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.0/aip-ocr-andoird-sdk-1.0.0.zip
+wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.0/aip-ocr-android-sdk-1.0.0.zip
 wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.0/aip-ocr-ios-sdk-1.0.0.zip
-
