951b35ccb101312d60d4a64f740c8361b893c615
songqingyun <songqingyun@baidu.com>
2017-03-21 18:47:32 +0800

    speech
    Change-Id: I59a6b38b5ead91703a2f84d826ee01c22e156b8d

--- external/app/brain/actions/tech/speech/Index.php
+++ external/app/brain/actions/tech/speech/Index.php
@@ -5,7 +5,7 @@
  * Date: 2017/3/6
  * Time: 下午3:09
  */
-class Action_Index extends Ap_Action_Abstract{
+class Action_Speech extends Ap_Action_Abstract{
     public function execute(){
         $arrPageInfo['page'] = "speech";
         Brain_Output::htmlOutput(
