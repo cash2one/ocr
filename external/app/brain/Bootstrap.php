3d5af1142130834d152dfcc58075bd0c11d9f0e6
songqingyun <songqingyun@baidu.com>
2017-03-21 17:28:27 +0800

    speech
    Change-Id: I96f14954611d8fa358784af8d1613911d351f6cc

--- external/app/brain/Bootstrap.php
+++ external/app/brain/Bootstrap.php
@@ -24,6 +24,9 @@ class Bootstrap extends Ap_Bootstrap_Abstract{
         $dispatcher->getRouter()->addRoute('tech_Antiporn', new Ap_Route_Rewrite('tech/antiporn',
             array("module"=>"tech", "controller"=>"antiporn", "action"=>"index")
         ));
+        $dispatcher->getRouter()->addRoute('tech_Speech', new Ap_Route_Rewrite('tech/speech',
+            array("module"=>"tech", "controller"=>"speech", "action"=>"index")
+        ));
     }
 
     /**
