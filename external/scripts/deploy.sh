65ca173a5c32b9aecc6f046d5cc8146ffc20e7f0
zhaojianxin <zhaojianxin@baidu.com>
2017-03-27 18:25:42 +0800

    temp update sdk
    Change-Id: Ifc8a435a7b9632db42ee9247512bf6158913f6f5

--- external/scripts/deploy.sh
+++ external/scripts/deploy.sh
@@ -1,5 +1,11 @@
 cd ../webroot/
 cd download
+
+rm -f aip-ocr-android-sdk-1.0.1.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.0/aip-ocr-android-sdk-1.0.1.zip
+rm -f aip-ocr-java-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-ocr-java-sdk-1.3.zip
+rm -f aip-antiporn-java-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-antiporn-java-sdk-1.3.zip
+rm -f aip-face-java-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-face-java-sdk-1.3.zip
+rm -f aip-nlp-java-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-nlp-java-sdk-1.3.zip
 rm -f aip-antiporn-php-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-antiporn-php-sdk-1.3.zip
 rm -f aip-face-php-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-face-php-sdk-1.3.zip
 rm -f aip-nlp-php-sdk-1.3.zip && wget ftp://cp01-zhanghongguang-dev.epc.baidu.com/1.3/aip-nlp-php-sdk-1.3.zip
