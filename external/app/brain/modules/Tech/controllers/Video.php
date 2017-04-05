7bdaa957e2bb16f550a3c6e0ce3476ebd570aa93
songqingyun <songqingyun@baidu.com>
2017-04-05 14:45:06 +0800

    video
    Change-Id: I264749acf5ef17d4e710cb780930a678d66f9c32

---
+++ external/app/brain/modules/Tech/controllers/Video.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by songqingyun@baidu.com.
+ * User: songqingyun
+ * Date: 2017/3/3
+ * Time: 下午6:54
+ */
+class Controller_Video extends Ap_Controller_Abstract
+{
+    public $actions = array(
+
+        'vcs' => 'actions/tech/video/Vcs.php',
+        'vca' => 'actions/tech/video/Vca.php',
+
+    );
+}
