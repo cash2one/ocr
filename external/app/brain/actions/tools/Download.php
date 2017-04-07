d9bdbbf40253356a1fdc24a706eb2e4078cbc273
wangyu61 <wangyu61@baidu.com>
2017-04-07 16:05:44 +0800

    debug8

--- external/app/brain/actions/tools/Download.php
+++ external/app/brain/actions/tools/Download.php
@@ -33,8 +33,8 @@ class Action_Download extends Ap_Action_Abstract
         $intAppid = '469';
         $strAppKey = 'ai.baidu.com';
         $intTmOut = '2000';
-        $strCookieDomain = 'ai.baidu.com';
-        $strLoginUrl = 'login.bcetest.baidu.com';
+        $strCookieDomain = '10.95.106.174';
+        $strLoginUrl = 'login.bce.baidu.com';
         $strJumpUrl = 'ai.baidu.com';
         $casInfo = new Cas_Info($arrServers, $intAppid, $strAppKey, $intTmOut);
         $casInfo->setCookieDomain($strCookieDomain);
