cb2a1fb0fbf66e528f8c328f1db101ad35d998d7
songqingyun <songqingyun@baidu.com>
2017-04-17 19:54:01 +0800

    删除新闻中无用接口
    Change-Id: I52a220b0a170833a32f2c9bd4d2582d789b2a8ce

---
+++ external/test/RedirectTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: songqingyun
+ * Date: 2017/4/17
+ * Time: 下午1:58
+ */
+class RedirectTest extends PHPUnit_Framework_TestCase
+{
+
+    public static function testRtrim(){
+        $url = rtrim("cloud.baidu.com/?1=1&fromai=1&service=SVA", "&");
+        echo $url;
+        return true;
+    }
+
+}
