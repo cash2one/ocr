413dacd0ae8bb83c0c6cc86c039009e5274f477c
songqingyun <songqingyun@baidu.com>
2017-04-18 16:01:36 +0800

    sdk download
    Change-Id: Ie2c41b4cdca6318b174e432f529616b4f2a9bf8f

---
+++ external/app/brain/models/dao/Sdk.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: songqingyun
+ * Date: 2017/4/17
+ * Time: 下午10:04
+ */
+class Dao_Sdk extends Dao_Base
+{
+    // 表名
+    private $strTable;
+    // 默认字段
+    private $arrDefaultFields = array(
+        'id',
+        'name',
+        'abs',
+        'version',
+        'file_path',
+        'pub_time',
+        'category',
+        'language',
+        'enable',
+    );
+
+    /**
+     * Dao_Sdk constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->strTable = 't_sdk';
+    }
+
+    /**
+     * 获取所有有效的sdk最多100个
+     */
+    public function getSdkList()
+    {
+        $arrFields = $this->arrDefaultFields;
+        $arrConds = array(
+            "enable=" => 1
+        );
+        $arrOptions = null;
+        $arrAppends = array(
+            'order by id desc ',
+            'limit 100',
+        );
+
+        $strSQL = $this->objSQLAssember->getSelect($this->strTable, $arrFields, $arrConds, $arrOptions, $arrAppends);
+
+        $arrDBRet = $this->query($strSQL);
+
+        return $arrDBRet;
+    }
+
+}
---
+++ external/app/brain/models/service/data/Sdk.php
@@ -0,0 +1,179 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: songqingyun
+ * Date: 2017/4/17
+ * Time: 下午10:00
+ */
+class Service_Data_Sdk
+{
+    private $sdkDao;
+    private $sdkCatDao;
+    private $sdkLanDao;
+    private $cacheSdkList = "AIP_WEB_SDK_LIST";
+    private $cacheSdkLanList = "AIP_WEB_SDK_LAN_LIST";
+    private $cacheSdkCatList = "AIP_WEB_SDK_CAT_LIST";
+
+    public function __construct()
+    {
+        $this->sdkDao = new Dao_Sdk();
+        $this->sdkLanDao = new Dao_SdkLan();
+        $this->sdkCatDao = new Dao_SdkCat();
+
+    }
+
+    /**
+     * 封装sdk数据给页面
+     * @return array
+     */
+    public function sdkData()
+    {
+        $sdkList = $this->sdkData->getSdkList();
+        $catList = $this->sdkData->getCatList();
+        $lanList = $this->sdkData->getLanList();
+
+        $result = array();
+
+        foreach ($sdkList as $sdk) {
+            $this->setSdkCat($catList, $sdk);
+            $this->setSdkType($lanList, $sdk);
+            $this->setSdkVersion($sdk);
+            $this->setSdkPubTime($sdk);
+            $result[$sdk['cat']][] = $sdk;
+        }
+        return $result;
+    }
+
+    /**
+     * 获取所有有效sdk
+     * @return mixed|void
+     */
+    public function getSdkList()
+    {
+        $val = Brain_Memcache::get($this->cacheSdkList);
+        if ($val && !empty($val)) {
+            return json_decode($val);
+        } else {
+            $sdkList = $this->sdkDao->getSdkList();
+            Brain_Memcache::set($this->cacheSdkList, json_encode($sdkList), 60 * 60);
+            return $sdkList;
+        }
+
+    }
+
+    /**设置 sdk 语言
+     * @param $lanList
+     * @param $sdk
+     */
+    public function setSdkType($lanList, $sdk)
+    {
+        if (!isset($sdk) || empty($lanList)) {
+            return;
+        }
+        foreach ($lanList as $lan) {
+            if ($sdk['language'] === $lan['id']) {
+                $sdk['type'] = $lan['name'];
+                return $sdk;
+            }
+        }
+        $sdk['type'] = "";
+        return $sdk;
+
+    }
+
+    /**
+     * 设置 sdk 分类
+     * @param $catList
+     * @param $sdk
+     */
+    public function setSdkCat($catList, $sdk)
+    {
+        if (!isset($sdk) || empty($catList)) {
+            return;
+        }
+        foreach ($catList as $cat) {
+            if ($sdk['category'] === $cat['id']) {
+                $sdk['cat'] = $cat['key'];
+                return $sdk;
+            }
+        }
+        $sdk['cat'] = "";
+        return $sdk;
+    }
+
+    /**
+     * 设置sdk 版本
+     * @param $sdk
+     */
+    public function setSdkVersion($sdk)
+    {
+        if (!isset($sdk)) {
+            return;
+        }
+        $version = (int)$sdk['version'];
+        $v = "";
+        $v .= $version / 1000 / 1000 % 1000 . ".";
+        $v .= $version / 1000 % 1000 . ".";
+        $v .= $version % 1000;
+        $sdk['version'] = $v;
+        return $sdk;
+    }
+
+    /**
+     * 设置 sdk 发布时间
+     * @param $sdk
+     */
+    public function setSdkPubTime($sdk)
+    {
+        if (!isset($sdk)) {
+            return;
+        }
+        $pubTime = $sdk['pub_time'];
+        $t = date_format($pubTime, "yyyy-MM-dd");
+        $sdk['pub_time'] = $t;
+        return $sdk;
+    }
+
+
+    /**
+     * 获取所有有效分类
+     * @return mixed|void
+     */
+    public function getCatList()
+    {
+        $val = Brain_Memcache::get($this->cacheSdkCatList);
+        if ($val && !empty($val)) {
+            return json_decode($val);
+        } else {
+            $list = $this->sdkCatDao->getList();
+            Brain_Memcache::set($this->cacheSdkCatList, json_encode($list), 60 * 60);
+            return $list;
+        }
+    }
+
+    /**
+     * 获取所有语言
+     * @return mixed|void
+     */
+    public function getLanList()
+    {
+        $val = Brain_Memcache::get($this->cacheSdkLanList);
+        if ($val && !empty($val)) {
+            return json_decode($val);
+        } else {
+            $list = $this->sdkLanDao->getList();
+            Brain_Memcache::set($this->cacheSdkLanList, json_encode($list), 60 * 60);
+            return $list;
+        }
+    }
+
+    /**
+     * 清除缓存
+     */
+    public function cleanCache()
+    {
+        Brain_Memcache::delete($this->cacheSdkList);
+    }
+
+}
