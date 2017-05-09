4b9c6a8818e26c6abf9cee16594a4eed1ac7ae2a
songqingyun <songqingyun@baidu.com>
2017-05-09 22:47:30 +0800

    首页banner图
    Change-Id: Ieba9dbbe2ce3ef1cf87c1dc7455b32deb8cee7cb

---
+++ external/app/brain/models/dao/BannerReview.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: songqingyun
+ * Date: 2017/4/17
+ * Time: 下午10:04
+ */
+class Dao_BannerReview extends Dao_Base
+{
+    // 表名
+    private $strTable;
+    // 默认字段
+    private $arrDefaultFields = array(
+        'id',
+        'value',
+        'dis_order',
+        'template_code',
+    );
+
+    /**
+     * Dao_Banner constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->strTable = 't_banner_review';
+    }
+
+    /**
+     * 获取Banner
+     * @return
+     */
+    public function getBannerReview()
+    {
+        $arrFields = $this->arrDefaultFields;
+        $arrConds = array(
+            'status=' => 1,
+        );
+        $arrOptions = null;
+        $arrAppends = array(
+            'order by dis_order asc, id desc',
+            'limit 20',
+        );
+        $strSQL = $this->objSQLAssember->getSelect($this->strTable, $arrFields, $arrConds, $arrOptions, $arrAppends);
+        $arrDBRet = $this->query($strSQL);
+        return $arrDBRet;
+    }
+}
---
+++ external/app/brain/models/service/data/Banner.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: wangyu61
+ * Date: 2017/3/8
+ * Time: 下午5:41
+ */
+class Service_Data_Doc
+{
+
+    protected $bannerDao;
+    protected $bannerReviewDao;
+    private $cacheBannerList = "AIP_WEB_BANNER_LIST";
+
+    public function __construct()
+    {
+        $this->bannerDao = new Dao_Banner();
+        $this->bannerReviewDao = new Dao_BannerReview();
+    }
+
+    /**
+     * 清除缓存
+     * @return
+     */
+    public function cleanCache()
+    {
+        Brain_Memcache::delete($this->cacheBannerList);
+    }
+
+    /**
+     * 获取首页banner图
+     * @return array
+     */
+    public function getBanner()
+    {
+        $val = Brain_Memcache::get($this->cacheBannerList);
+        if ($val && !empty($val)) {
+            return Bd_String::json_decode($val, true);
+        } else {
+            $list = $this->bannerDao->getBanner();
+            Brain_Memcache::set($this->cacheBannerList, Bd_String::json_encode($list), 60 * 60);
+            return $list;
+        }
+    }
+
+    /**
+     * 获取预览banner
+     * @return array
+     */
+    public function getBannerReview(){
+        return $this->bannerReviewDao->getBannerReview();
+    }
+
+    /**
+     * 合并预览和在线banner
+     * @return array
+     */
+    public function mergeBanner(){
+        $reviews = $this->getBannerReview();
+        $banners = $this->getBanner();
+        if (empty($reviews)){
+            return $banners;
+        }
+
+    }
+
+}
